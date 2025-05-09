e66174322307b6b223e4c98d01919360dcaac9d1
melvinshook <139077191+melvinshook@users.noreply.github.com>
2025-05-09 13:05:32 -0400

    fixe

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/", // replace with your GitHub repo name
+  base: "/my-portfolio/", // MUST match the repo name
 });
