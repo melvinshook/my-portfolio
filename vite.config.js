47a9f58c2d4d251dd3c2c5fe53871d708a5c5331
melvinshook <139077191+melvinshook@users.noreply.github.com>
2025-05-09 13:00:04 -0400

    spelling mistake fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfoli/", // replace with your GitHub repo name
+  base: "/my-portfolio/", // replace with your GitHub repo name
 });
