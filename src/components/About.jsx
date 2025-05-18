907527ba7f87acd089c36a3ecd2c29a2b1c9cace
melvinshook <139077191+melvinshook@users.noreply.github.com>
2025-05-18 19:14:49 -0400

    updates to styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,16 +3,18 @@ import styles from "./About.module.css"; // keep if you want scoped styles
 
 function About() {
   return (
-    <main className={styles.aboutSection}>
-      <h2 className={styles.title}>About Me</h2>
-      <div className={styles.content}>
-        <p>
-          I’m a passionate photographer and videographer based in Columbia, MD.
-        </p>
-        <p>Let's turn your visions into reality</p>
-        {/* More text here */}
+    <section className={styles.aboutSection}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>About Me</h2>
+        <div className={styles.content}>
+          <p>
+            I’m a passionate photographer and videographer based in Columbia,
+            MD.
+          </p>
+          <p>Let's turn your visions into reality.</p>
+        </div>
       </div>
-    </main>
+    </section>
   );
 }
 
